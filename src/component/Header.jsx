f23ff6a1000e37e9f752301b4829e1a0714fa887
Erremme <Erremme2.0@gmail.com>
2025-01-28 17:58:12 +0100

    feat : add page header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,3 +1,31 @@
 export default function Header (){
-    return <header><h1>Header</h1></header>;
+    return(
+     <header>
+        <div className="container">
+           <div className="row-header d-flex">
+              <div className="col-left text-center">
+                <img className="img-fluid" src="../img/dc-logo.png" alt="Logo Dc" />
+              </div>
+              <div className="col-right d-flex">
+                 <nav className="header-navbar">
+                    <ul className="d-flex">
+                        <li><a href="#">characters</a></li>
+                        <li><a className="active" href="#">comics</a><div className="line-bar"></div></li>
+                        <li><a href="#">movies</a></li>
+                        <li><a href="#">tv</a></li>
+                        <li><a href="#">games</a></li>
+                        <li><a href="#">collectibles</a></li>
+                        <li><a href="#">videos</a></li>
+                        <li><a href="#">fans</a></li>
+                        <li><a href="#">news</a></li>
+                        <li><a href="#">shop</a></li>
+                        
+                    </ul>
+                 </nav>
+              </div>
+
+            </div> 
+        </div>     
+    </header>
+    );
 }
