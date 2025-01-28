c92143b078c970c72eda6d55a1df3dc8d7b33243
Erremme <Erremme2.0@gmail.com>
2025-01-28 21:03:30 +0100

    feat: add footer center

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer (){
                     <div className="container">
                         <nav className="footer-nav ">
                           <ul className="d-flex">
-                            <li><img className="img-fluid" src="../img/buy-comics-digital-comics.png" alt="digital comics" /><a href="#">digital comics</a></li>
+                                  <li><img className="img-fluid" src="../img/buy-comics-digital-comics.png" alt="digital comics" /><a href="#">digital comics</a></li>
                             <li><img className="img-fluid" src="../img/buy-comics-merchandise.png" alt="merchandise" /><a href="#">dc merchandise</a></li>
                             <li><img className="img-fluid" src="../img/buy-comics-subscriptions.png" alt="subscriptions" /><a href="#">subscription</a></li>
                             <li><img className="img-fluid" src="../img/buy-comics-shop-locator.png" alt="shop" /><a href="#">comic shop locator</a></li>
@@ -16,6 +16,62 @@ export default function Footer (){
                         </nav>
                     </div>
                 </div>  
+                <div className="footer-center">
+                  <div className="container">
+                    <div className="footer-center-row d-flex">
+                       <div className="col-left d-flex">
+                          <nav>
+                            <h5>DC COMICS</h5>
+                            <ul>
+                              <li><a href="#">Characteres</a></li>
+                              <li><a href="#">Comics</a></li>
+                              <li><a href="#">Movies</a></li>
+                              <li><a href="#">TV</a></li>
+                              <li><a href="#">Games</a></li>
+                              <li><a href="#">Videos</a></li>
+                              <li><a href="#">News</a></li>
+                            </ul>
+
+                             <h5 className="shop">SHOP</h5>
+                             <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectbles</a></li>
+                              </ul>
+                          </nav>
+                          <nav>
+                             <h5>DC</h5>
+                             <ul>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy Policy (New) </a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                          
+                              </ul>
+                            </nav>
+                            <nav>
+                              <h5>SITES</h5>
+                              <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                              </ul>
+                            </nav>
+                      </div> 
+                      <div className="col-right">
+                         <img className="img-fluid" src="../img/dc-logo-bg.png" alt="Logo DC" />
+                        </div>            
+                    </div>
+                  </div>
+                </div>
             </footer>
     );
 }
